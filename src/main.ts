8ec1d998edb30d16efe2bbbee981c6ff1252a280
Meet Soni <meet.soni@mytegroup.com>
2024-11-09 18:05:05 +0530

    enable helmet

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-// import helmet from 'helmet';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.use(helmet());
+  app.use(helmet());
 
   app.enableCors({ origin: '*' });
 
